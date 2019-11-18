7321b9e5e0e001993ac9ba75f06d9ec1ead90252
Michael Williams <michael.williams@dvla.gov.uk>
2019-11-18 18:04:26 +0000

    First Selenium test is now working, but failing due to clicking the incorrect button.

--- src/test/java/selenium/SeleniumTests.java
+++ src/test/java/selenium/SeleniumTests.java
@@ -11,6 +11,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 
 public class SeleniumTests {
 
